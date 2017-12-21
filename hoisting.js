584eba2c7b75a4942ac58a549a73e304691dc135
Jason Vance <jbvance@gmail.com>
2017-12-21 00:04:14 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,31 +1,29 @@
 function callMe() {
-  var lyric = "maybe";
-  console.log("I just met you...");
-  console.log("and this is crazy..");
+  var lyric = 'maybe';
+  console.log('I just met you...');
+  console.log('and this is crazy..');
   console.log("but here's my number..");
-  console.log("so call me");
+  console.log('so call me');
   return lyric;
-  
 }
-
 
 function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
+  var thisIsCrazy = function() {
+    console.log('hey!!!');
+  };
 }
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
+  var name = 'Cricky';
 
   sayMy();
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
+    var name = 'Kristin';
   }
 }
